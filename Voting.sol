b473ec5739fa77e02f2eef41695e2588cc6cde25
Dang Quoc Huy <huysamdua@yahoo.com>
2018-07-10 14:55:06 +0700

    Step 1: Build the Contract

---
+++ Voting.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.18;
+
+contract Voting {
+  address public owner;
+
+  function Voting() public {
+    owner = msg.sender;
+  }
+
+  function kill() public {
+    if(msg.sender == owner) {
+      selfdestruct(owner);
+    }
+  }
+}
