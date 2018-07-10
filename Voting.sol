5ea7a173c55a69e9301987e35e1260f9109ee457
Dang Quoc Huy <huysamdua@yahoo.com>
2018-07-10 15:54:34 +0700

    Step 3: Add in voting logic

--- Voting.sol
+++ Voting.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.4.18;
 contract Voting {
   address public owner;
 
+  mapping (bytes32 => uint8) public votesReceived;
+  bytes32[] public candidateList;
+
   // The function body is inserted where the special symbol
   // `_;` in the definition of a modifier appears.
   // This means that if the owner calls this function, the
@@ -16,8 +19,13 @@ contract Voting {
     _;
   }
 
-  function Voting() public {
+  function Voting(bytes32[] candidateNames) public {
+    candidateList = candidateNames;
     owner = msg.sender;
+  }
+
+  function voteForCandidate(bytes32 candidate) public {
+    votesReceived[candidate] += 1;
   }
 
   function kill() public onlyOwner {
