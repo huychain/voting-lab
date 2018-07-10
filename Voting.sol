02db2b576710ea56316b5541e02071b0b2ffa133
Dang Quoc Huy <huysamdua@yahoo.com>
2018-07-10 16:02:06 +0700

    guard out of index

--- Voting.sol
+++ Voting.sol
@@ -25,10 +25,20 @@ contract Voting {
   }
 
   function voteForCandidate(bytes32 candidate) public {
+    assert(validCandidate(candidate) == true);
     votesReceived[candidate] += 1;
   }
 
   function kill() public onlyOwner {
     selfdestruct(owner);
+  }
+
+  function validCandidate(bytes32 candidate) returns (bool) {
+    for(uint i = 0; i < candidateList.length; i++) {
+      if (candidateList[i] == candidate) {
+        return true;
+      }
+    }
+    return false;
   }
 }
