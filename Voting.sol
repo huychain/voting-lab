769ca47e237a5536e9e95909e9b47f7ffb4c0422
Dang Quoc Huy <huysamdua@yahoo.com>
2018-07-10 15:22:59 +0700

    Step 2: Build and Deploy the Contract

--- Voting.sol
+++ Voting.sol
@@ -3,13 +3,24 @@ pragma solidity ^0.4.18;
 contract Voting {
   address public owner;
 
+  // The function body is inserted where the special symbol
+  // `_;` in the definition of a modifier appears.
+  // This means that if the owner calls this function, the
+  // function is executed and otherwise, an exception is
+  // thrown.
+  modifier onlyOwner {
+    require(
+      msg.sender == owner,
+      "Only owner can call this function."
+    );
+    _;
+  }
+
   function Voting() public {
     owner = msg.sender;
   }
 
-  function kill() public {
-    if(msg.sender == owner) {
-      selfdestruct(owner);
-    }
+  function kill() public onlyOwner {
+    selfdestruct(owner);
   }
 }
